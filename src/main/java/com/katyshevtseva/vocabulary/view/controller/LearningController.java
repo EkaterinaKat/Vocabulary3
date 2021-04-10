82ebdd4cd1ee2ecf47fa2ccf3099fbef9bf3485e
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2021-04-10 18:25:59 +0300

    LevelsInfo в окне изучения

--- src/main/java/com/katyshevtseva/vocabulary/view/controller/LearningController.java
+++ src/main/java/com/katyshevtseva/vocabulary/view/controller/LearningController.java
@@ -10,7 +10,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.katyshevtseva.fx.Utils.closeWindowThatContains;
 
@@ -31,6 +33,8 @@ class LearningController implements FxController {
     private Button okButton;
     @FXML
     private Button notOkButton;
+    @FXML
+    private Label levelsInfoLabel;
 
     LearningController(List<Entry> entries) {
         this.entries = entries;
@@ -45,6 +49,7 @@ class LearningController implements FxController {
         showTranslationButton.setOnAction(event -> showTranslationButtonListener());
         wordCount = -1;
         nextWord();
+        setLevelsInfo();
     }
 
     private void resultButtonsListener(boolean positiveAnswer) {
@@ -93,5 +98,17 @@ class LearningController implements FxController {
         VocUtils.getDialogBuilder().setDialogHeight(600).openInfoDialog("Learning is completed!\n\n"
                 + Core.getInstance().learningService().getStatisticsReport());
         closeWindowThatContains(wordLabel);
+    }
+
+    private void setLevelsInfo() {
+        Map<Integer, Integer> levelNumOfEntriesMap = new HashMap<>();
+        entries.forEach(entry ->
+                levelNumOfEntriesMap.put(entry.getLevel(), levelNumOfEntriesMap.getOrDefault(entry.getLevel(), 0) + 1));
+        StringBuilder levelsInfo = new StringBuilder("[");
+        levelNumOfEntriesMap.keySet().forEach(level ->
+                levelsInfo.append(String.format("%d:%d ", level, levelNumOfEntriesMap.get(level))));
+        levelsInfo.deleteCharAt(levelsInfo.length() - 1);
+        levelsInfo.append("]");
+        levelsInfoLabel.setText(levelsInfo.toString());
     }
 }
